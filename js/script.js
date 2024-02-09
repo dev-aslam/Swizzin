e0209bae52d04433cdcb0efc3cf001fbdd36c2cd
Muhammed Aslam <muhammedaslam204@gmail.com>
2024-02-09 11:48:54 +0530

    Completed the navbar

--- js/script.js
+++ js/script.js
@@ -1,13 +1,57 @@
+const navbar = document.querySelector('.navbar');
+const navbarCollapsed = document.querySelector('.offcanvas');
+
+const gitdark = document.querySelectorAll('.github-dark');
+const gitlight = document.querySelectorAll('.github-light');
+
+const crosslight = document.querySelector('.cross-light');
+const crossdark = document.querySelector('.cross-dark');
+
+const lightMode = document.querySelectorAll('.lightToggleIcon');
+const darkMode = document.querySelectorAll('.darkToggleIcon');
+
 function darkLightToggler(){
-    var navbar = document.querySelector('.navbar');
     if(navbar.classList.contains('navbar-dark')){
+
         navbar.classList.remove('navbar-dark');
-        document.querySelectorAll(".github-light").forEach.classList.remove('github-display');
-        document.querySelectorAll(".github-dark").forEach.classList.add('github-display');
+        navbarCollapsed.classList.remove('navbar-dark');
+        crosslight.classList.remove('display-none');
+        crossdark.classList.add('display-none');
+
+        gitlight.forEach(element => {
+            element.classList.remove('display-none');
+        });
+        gitdark.forEach(element=>{
+            element.classList.add('display-none');
+        });
+        
+        lightMode.forEach(element => {
+            element.classList.remove('display-none');
+        });
+
+        darkMode.forEach(element => {
+            element.classList.add('display-none');
+        });
     }
     else{
         navbar.classList.add('navbar-dark');
-        document.querySelectorAll(".github-light").forEach.classList.add('github-display');
-        document.querySelectorAll(".github-dark").forEach.classList.remove('github-display');
+        navbarCollapsed.classList.add('navbar-dark');
+        crosslight.classList.add('display-none');
+        crossdark.classList.remove('display-none');
+
+        gitlight.forEach(element => {
+            element.classList.add('display-none');
+        });
+        gitdark.forEach(element=>{
+            element.classList.remove('display-none');
+        });
+
+        lightMode.forEach(element => {
+            element.classList.add('display-none');
+        });
+
+        darkMode.forEach(element => {
+            element.classList.remove('display-none');
+        });
     }
 }
