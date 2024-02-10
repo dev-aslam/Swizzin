a5aabefb03a3b666f2388956f4393427349a75b4
Muhammed Aslam <muhammedaslam204@gmail.com>
2024-02-10 12:38:28 +0530

    created different css files for dark and light mode, implemented dark mode toggle

--- js/script.js
+++ js/script.js
@@ -1,85 +1,11 @@
-//NavBar Selector
-const navbar = document.querySelector('.navbar');
-const navbarCollapsed = document.querySelector('.offcanvas');
-
-//Github Icons(2 Icon)
-const gitdark = document.querySelectorAll('.github-dark');
-const gitlight = document.querySelectorAll('.github-light');
-
-//Navbar Collapsed Cross icon
-const crosslight = document.querySelector('.cross-light');
-const crossdark = document.querySelector('.cross-dark');
-
-//Dark and light mode icons
-const lightMode = document.querySelectorAll('.lightToggleIcon');
-const darkMode = document.querySelectorAll('.darkToggleIcon');
-
-//Body Dark and dark mode
-const bodylightShade = document.querySelectorAll('.section-light');
-const bodyDarkShade = document.querySelectorAll('.section-dark');
-
-function darkLightToggler(){
-    if(navbar.classList.contains('navbar-dark')){
-
-        navbar.classList.remove('navbar-dark');
-        navbarCollapsed.classList.remove('navbar-dark');
-        crosslight.classList.remove('display-none');
-        crossdark.classList.add('display-none');
-
-        gitlight.forEach(element => {
-            element.classList.remove('display-none');
-        });
-        gitdark.forEach(element=>{
-            element.classList.add('display-none');
-        });
-        
-        lightMode.forEach(element => {
-            element.classList.remove('display-none');
-        });
-
-        darkMode.forEach(element => {
-            element.classList.add('display-none');
-        });
-
-        bodylightShade.forEach(element => {
-            element.classList.remove('dark-mode-light');
-        });
-
-        bodyDarkShade.forEach(element => {
-            element.classList.remove('dark-mode-dark');
-        });
-    }
-    else{
-        navbar.classList.add('navbar-dark');
-        navbarCollapsed.classList.add('navbar-dark');
-        crosslight.classList.add('display-none');
-        crossdark.classList.remove('display-none');
-
-        gitlight.forEach(element => {
-            element.classList.add('display-none');
-        });
-        gitdark.forEach(element=>{
-            element.classList.remove('display-none');
-        });
-
-        lightMode.forEach(element => {
-            element.classList.add('display-none');
-        });
-
-        darkMode.forEach(element => {
-            element.classList.remove('display-none');
-        });
-
-        bodylightShade.forEach(element => {
-            element.classList.add('dark-mode-light');
-        });
-
-        bodyDarkShade.forEach(element => {
-            element.classList.add('dark-mode-dark');
-        });
-    }
+//function to toggle dark mode
+function toggleDarkMode() {
+    const isDarkMode = document.body.classList.toggle('dark-mode');
+    const stylesheet = isDarkMode ? './css/dark.css' : './css/light.css';
+    document.getElementById('theme-style').setAttribute('href', stylesheet);
 }
 
+//function to copy text
 function copyCode(elementId) {
     var codeElement = document.getElementById(elementId);
     var textArea = document.createElement("textarea");
@@ -91,6 +17,8 @@ function copyCode(elementId) {
     showCopySuccessIcon(elementId);
 }
 
+
+//function to show check mark after pressing copy
 function showCopySuccessIcon(buttonId) {
     var button = document.querySelector(`button[onclick="copyCode('${buttonId}')"`);
     var successIcon = button.querySelector(".copyButtonSuccessIcon");
@@ -100,5 +28,5 @@ function showCopySuccessIcon(buttonId) {
     setTimeout(function() {
         successIcon.style.display = "none";
         copyIcon.style.display = "inline-block";
-    }, 2000);
+    }, 1000);
 }
